40d2f5bc20616f1f2768de7a1f9bb2eac0d30afc
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-09-08 12:52:17 +0200

    created logoutGetUser controller

--- src/controllers/users/index.controller.js
+++ src/controllers/users/index.controller.js
@@ -1,14 +1,17 @@
 import makeLoginGetUser from "./login-get-user.controller.js";
+import makeLogoutGetUser from "./logout-get-user.controller.js";
 import makePostUser from "./post-user.controller.js";
 import usersUseCase from "../../use-cases/users/index.use-case.js";
 
 const loginUser = makeLoginGetUser(usersUseCase.loginUser);
+const logoutUser = makeLogoutGetUser(usersUseCase.logoutUser);
 const postUser = makePostUser(usersUseCase.createUser);
 
 const usersController = Object.freeze({
   loginUser,
+  logoutUser,
   postUser,
 });
 
 export default usersController;
-export { postUser, loginUser };
+export { loginUser, logoutUser, postUser };
