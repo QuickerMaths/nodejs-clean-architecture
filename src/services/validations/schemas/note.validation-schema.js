4742a57dcba12785e16362f0beaaa4efe88d2bc4
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-09-12 22:46:43 +0200

    added userId to notes.validation-schema

--- src/services/validations/schemas/note.validation-schema.js
+++ src/services/validations/schemas/note.validation-schema.js
@@ -4,7 +4,8 @@ export default {
     title: { type: "string", minLength: 10, maxLength: 100 },
     content: { type: "string", minLength: 10, maxLength: 1000 },
     important: { type: "boolean" },
+    userId: { type: "string" },
   },
-  required: ["title", "content"],
+  required: ["title", "content", "userId"],
   additionalProperties: false,
 };
