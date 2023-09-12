ce8145d69404fc5159bf8859a175c9386faac272
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-09-12 23:26:10 +0200

    added remove and updated methods to notes data-access layer

--- src/data-access/notes/notes.db.js
+++ src/data-access/notes/notes.db.js
@@ -11,8 +11,22 @@ export default function makeNotesDb(model) {
     return note;
   }
 
+  async function remove({ id }) {
+    const note = await model.findByIdAndDelete(id);
+
+    return note;
+  }
+
+  async function update({ id, ...noteData }) {
+    const note = await model.findByIdAndUpdate(id, { ...noteData });
+
+    return note;
+  }
+
   return Object.freeze({
     findAllByUserId,
     insert,
+    remove,
+    update,
   });
 }
