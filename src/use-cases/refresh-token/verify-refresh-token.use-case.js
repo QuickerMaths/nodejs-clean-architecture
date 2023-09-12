985251a1c7bf2850656f4cb45e2f042ff039e30f
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-09-12 22:36:40 +0200

    refactored verify-refresh-token.use-case to reduce redundancy

--- src/use-cases/refresh-token/verify-refresh-token.use-case.js
+++ src/use-cases/refresh-token/verify-refresh-token.use-case.js
@@ -1,16 +1,18 @@
 import { ForbiddenError } from "../../utils/errors/ForbiddenError.js";
 
+async function handleForbiddenError(message, removeToken = false) {
+  if (removeToken)
+    await refreshTokenDb.removeByProperty({ userId: decodedToken.id });
+
+  throw new ForbiddenError("Forbidden.", 403, message, true);
+}
+
 export default function makeVerifyRefreshToken(refreshTokenDb, authService) {
   return async function verifyRefreshToken(requestToken) {
     const decodedToken = authService.jwt.decodeToken(requestToken);
 
     if (!decodedToken) {
-      throw new ForbiddenError(
-        "Forbidden.",
-        403,
-        "Invalid refresh token.",
-        true
-      );
+      await handleForbiddenError("Invalid refresh token.");
     }
 
     const isTokenInDb = await refreshTokenDb.findByProperty({
@@ -18,41 +20,20 @@ export default function makeVerifyRefreshToken(refreshTokenDb, authService) {
     });
 
     if (!isTokenInDb) {
-      throw new ForbiddenError(
-        "Forbidden.",
-        403,
-        "Invalid refresh token.",
-        true
-      );
+      await handleForbiddenError("Invalid refresh token.");
     } else if (isTokenInDb.token !== requestToken) {
-      await refreshTokenDb.remove({ token: isTokenInDb.token });
-      throw new ForbiddenError(
-        "Forbidden.",
-        403,
-        "Invalid refresh token.",
-        true
-      );
+      await handleForbiddenError("Invalid refresh token.", true);
     }
 
     const verificationResult = await authService.jwt.verifyRefreshToken(
       requestToken
     );
 
-    if (!verificationResult || verificationResult === "expired") {
-      await refreshTokenDb.remove({ token: requestToken });
-
-      if (verificationResult === "expired") {
-        throw new ForbiddenError(
-          "Forbidden.",
-          403,
-          "Refresh token expired. Please log in again.",
-          true
-        );
-      }
-      throw new ForbiddenError(
-        "Forbidden.",
-        403,
-        "Invalid refresh token.",
+    if (!verificationResult) {
+      await handleForbiddenError("Invalid refresh token.", true);
+    } else if (verificationResult === "expired") {
+      await handleForbiddenError(
+        "Refresh token expired please login again.",
         true
       );
     }
