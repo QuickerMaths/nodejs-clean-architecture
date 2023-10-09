a971a152e45e5c483a40c292afc1ddb62b87360f
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-10-09 23:56:57 +0200

    minot fixes

--- db/index.js
+++ db/index.js
@@ -1,8 +1,5 @@
-import dotenv from "dotenv";
 import mongoose from "mongoose";
 import config from "../src/config/config.js";
-
-dotenv.config();
 
 mongoose.connect(config.db.url);
 
--- src/config/config.js
+++ src/config/config.js
@@ -2,15 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  port: process.env.PORT || 3000,
+  port: process.env.NODE_LOCAL_PORT || 3000,
   ip: process.env.HOST || "0.0.0.0",
   db: {
-    url: process.env.DB_URI || "mongodb://localhost:27017/notes",
+    url: process.env.DB_URI || "mongodb://root:example@localhost:27017/"
   },
   jwt: {
     jwtSecret: process.env.JWT_SECRET || "secret",
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || "refresh-secret",
     jwtExpiration: 3600, // 1 hour
-    jwtRefreshExpiration: 86400, // 24 hours
-  },
+    jwtRefreshExpiration: 86400 // 24 hours
+  }
 };
